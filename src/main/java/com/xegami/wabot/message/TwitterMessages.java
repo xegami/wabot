9483b6c0e50fd6356eee3d1f0c782a73f1cd2eb9
Enmanuel (Xegami) Domínguez <devetoper@gmail.com>
2019-02-28 10:40:07 +0100

    twitter fixes

--- src/main/java/com/xegami/wabot/message/TwitterMessages.java
+++ src/main/java/com/xegami/wabot/message/TwitterMessages.java
@@ -7,10 +7,13 @@ public class TwitterMessages extends BaseMessages {
     public static String tuit(Status status) {
         String message = "";
         String url = "https://twitter.com/" + status.getUser().getScreenName() + "/status/" + status.getId();
+        String text = status.getText()
+                .replace("\n", n())
+                .replaceAll("http.*", "");
 
         message += "*" + status.getUser().getName() + "* @" + status.getUser().getScreenName() + n()
                 + n()
-                + "<<" + status.getText().replace("\n", n()) + ">>" + n()
+                + text + n()
                 + n()
                 + url;
 
